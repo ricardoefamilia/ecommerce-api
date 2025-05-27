7b75377e430ea4b0fa2e7c7048c720284ce5afa8
ricardoefamilia <ricardoefamilia@gmail.com>
2025-05-27 14:15:43 -0300

    install tsc-watch

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Bem-vindo ao curso de Node.js!");    
+  res.send("Bem-vindo ao curso de Node.js! - tsc-watch");    
 });
 
 app.listen(3000, () => {
