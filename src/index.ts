b054549042117b06c3a76d9be52d6e11d5f60c56
ricardoefamilia <ricardoefamilia@gmail.com>
2025-05-28 10:37:36 -0300

    Aula métodos get e post no TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-import express from "express";
+import express, {Request, Response} from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Bem-vindo ao curso de Node.js! - tsc-watch");    
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bem-vindo ao curso de Node.js! - Curso em Vídeo");    
+});
+
+
+let users = [
+    {id: 1, name: "João", age: 25},
+    {id: 2, name: "João", age: 35}
+  ];
+  
+app.get("/users", (req: Request, res: Response) => {
+  
+  // Simulando uma consulta ao banco de dados
+  res.send(users);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  // Simulando a criação de um usuário  
+  let user = req.body;
+  users.push(user);
+  res.send({
+    message: "Usuário criado com sucesso!",
+    user: user
+  });
 });
 
 app.listen(3000, () => {
